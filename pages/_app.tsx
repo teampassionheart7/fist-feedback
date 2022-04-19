fb6e7293fa082ffb9b31a0605aa2624b4224d215
greatSumini <greatSumini@gmail.com>
2022-04-19 16:01:09 +0900

    build(): set recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { RecoilRoot } from "recoil";
 
 import "antd/dist/antd.css";
 
@@ -10,7 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>FIST Feedback</title>
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   );
 }
