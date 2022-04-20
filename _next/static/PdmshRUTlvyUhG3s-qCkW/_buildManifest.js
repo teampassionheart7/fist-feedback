208632d3ad8d421afe6be871a0ce480e652b6338
greatSumini <greatSumini@gmail.com>
2022-04-20 04:53:00 +0000

    Deploying to gh-pages from @ teampassionheart7/fist-feedback@270cdec0750174d218a5c0d1109d25c715f79aa4 🚀

--- _next/static/PdmshRUTlvyUhG3s-qCkW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-289ba7b5fb63f228.js","static/chunks/795-be5f31c78563a45c.js","static/chunks/pages/index-3c6c2943fad7f476.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
